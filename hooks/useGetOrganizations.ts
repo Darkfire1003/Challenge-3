a5a5e070bd1f66d22eeb67e95861ca2ee1280657
Peter Gabriel <peter0gabriel@gmail.com>
2026-08-02 10:37:34 +0200

    changed usegetorg

--- hooks/useGetOrganizations.ts
+++ hooks/useGetOrganizations.ts
@@ -1,12 +1,18 @@
 import { useQuery } from "@tanstack/react-query";
 import { api, Organization } from "@/app/api/config";
+import { useAuth } from "@/app/context/AuthContext";
 
 export function useGetOrganizations() {
+  const { accessToken } = useAuth();
+
   return useQuery({
     queryKey: ["organizations"],
     queryFn: async () => {
-      const { data } = await api.get<Organization[]>("/organizations");
+      const { data } = await api.get<Organization[]>("/organizations", {
+        headers: { Authorization: `Bearer ${accessToken}` },
+      });
       return data;
     },
+    enabled: !!accessToken,
   });
 }
