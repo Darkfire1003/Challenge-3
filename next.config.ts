87959602a2b0175f1da348fe7e0dec7714d259ef
Kuscher Michael <michael.kuscher90@gmail.com>
2026-08-13 10:44:21 +0200

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,15 @@
 import type { NextConfig } from "next";
 
 const isGitHubActions = process.env.GITHUB_ACTIONS === "true";
-
 const basePath = isGitHubActions ? "/challenge-3" : "";
 
 const nextConfig: NextConfig = {
   output: "export",
-
   basePath,
-
   assetPrefix: isGitHubActions ? `${basePath}/` : "",
-
   trailingSlash: true,
-
-  env: {
-    NEXT_PUBLIC_BASE_PATH: basePath,
-  },
-
   images: {
     unoptimized: true,
-
     remotePatterns: [
       {
         protocol: "https",
